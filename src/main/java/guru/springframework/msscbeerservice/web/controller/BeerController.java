20f88d626caf41dfd4acff6ee2e310141dfab6cf
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-29 02:08:06 +0200

    adding get, post and put methods in controller

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,29 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<HttpStatus> saveNewBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<HttpStatus> updateBeer(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
+
