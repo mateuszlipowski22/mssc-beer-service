1d2a925ae55a88fbc33e2aec9e0757b0d80c1abc
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-29 02:00:32 +0200

    adding data model

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+    private String name;
+
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+
+}
