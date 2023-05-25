149b703c818cf86cd5089c327fff018962481d1b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-25 02:32:14 +0200

    Refactor Model to Common Package

--- src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -1,8 +1,8 @@
 package guru.springframework.msscbeerservice.services;
 
-import guru.springframework.msscbeerservice.web.model.BeerDto;
-import guru.springframework.msscbeerservice.web.model.BeerPageList;
-import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import guru.sfg.brewery.model.BeerDto;
+import guru.sfg.brewery.model.BeerPageList;
+import guru.sfg.brewery.model.BeerStyleEnum;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.UUID;
